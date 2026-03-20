afdcd20d6d369cf6f63f178c99cd434efb401a51
LukeJeppsen <lukepj12@gmail.com>
2026-03-20 12:34:17 -0600

    Database + connection string

--- RVfamcamp/RVfamcamp/Program.cs
+++ RVfamcamp/RVfamcamp/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// For database
+AppDomain.CurrentDomain.SetData("DataDirectory", Directory.GetCurrentDirectory());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
