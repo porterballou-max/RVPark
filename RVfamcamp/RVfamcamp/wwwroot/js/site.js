6f2d8f3c92bc789b7983d2bdb526285b9e7e2e39
Rick <RTSmith801@gmail.com>
2026-03-20 11:53:51 -0600

    Layout and Styles overhaul

--- RVfamcamp/RVfamcamp/wwwroot/js/site.js
+++ RVfamcamp/RVfamcamp/wwwroot/js/site.js
@@ -1,7 +1,10 @@
 ﻿// script to unlock the payments, reservations, profile pages after submit has been clicked
 // on the login page
-document.getElementById("loginLink").addEventListener("click", function() {
-    doucment.getElementById("paymentsLink").removeAttribute("disabled");
-    document.getElementById("reservationsLink").removeAttribute("disabled");
-    document.getElementById("profileLink").removeAttribute("disabled");
-});
+
+// Commenting out for time being. Will revisit once login is flushed out. 
+
+//document.getElementById("loginLink").addEventListener("click", function() {
+//    doucment.getElementById("paymentsLink").removeAttribute("disabled");
+//    document.getElementById("reservationsLink").removeAttribute("disabled");
+//    document.getElementById("profileLink").removeAttribute("disabled");
+//});
