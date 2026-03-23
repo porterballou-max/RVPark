b9849628f9e6eedede2b5b4bc994bea2cedd2adb
JordanPetty <jordanpetty@mail.weber.edu>
2026-03-22 18:14:25 -0600

    Added cookies for persistent login, as well as actually checking login information against database for proper login. If user logs in and then closes the web page, they should still be logged in when reopened. Also added logout button and page for logging out. Recommended next step is to move pages into folders so then cookies can be used to determine whether user should have access to pages in those folders (as opposed to indicating it on every page).

--- RVfamcamp/RVfamcamp/Program.cs
+++ RVfamcamp/RVfamcamp/Program.cs
@@ -1,7 +1,25 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// We love cookies
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options=>
+    {
+        options.Cookie.HttpOnly = true;     // Prevents Javascript
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;    // Only use on HTTPS
+        options.Cookie.SameSite = SameSiteMode.Lax;     // Keeps cookie just on our site
+        options.Cookie.IsEssential = true;      // For compliance with some European privacy law
+
+        options.ExpireTimeSpan = TimeSpan.FromDays(14);     // Cookie expires 14 days after being created
+        options.SlidingExpiration = true;       // Allows cookie expiration to move if use logs in again
+        options.LoginPath = "/Login";            // Where to redirect user if not logged in (if cookie expired)
+        options.AccessDeniedPath = "/Login";     // Where to redirect user if cookie fails (i.e. password changed)
+        options.ReturnUrlParameter = "ReturnUrl";
+    });
+    
 
 // For database
 AppDomain.CurrentDomain.SetData("DataDirectory", Directory.GetCurrentDirectory());
@@ -16,9 +34,12 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseCookiePolicy();      
+app.UseAuthentication();    // For Cookies
 app.UseAuthorization();
 
 app.MapStaticAssets();
