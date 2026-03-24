577e9cf8f7fe1e184be0c2e905c37005a3bd93ee
LukeJeppsen <lukepj12@gmail.com>
2026-03-23 20:20:31 -0600

    Update Lot.cs

--- RVfamcamp/RVfamcamp/Models/Lot.cs
+++ RVfamcamp/RVfamcamp/Models/Lot.cs
@@ -3,6 +3,7 @@ namespace RVfamcamp.Models;
 public class Lot
 {
     public int LotId { get; set; }
+    public int LotNumber { get; set; }
     public bool IsOccupied { get; set; }
     public int LotType { get; set; }
 }
