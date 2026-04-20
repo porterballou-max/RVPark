1c484d1161a4d8266160bbd85a9c9631d73eb68e
grantandrus <grantandrus2004@gmail.com>
2026-04-19 22:43:02 -0600

    feat: updated admin privalges, enabled editing, and fixed isoccupied bugs

--- RVfamcamp/RVfamcamp/Pages/Reservations/Edit.cshtml.cs
+++ RVfamcamp/RVfamcamp/Pages/Reservations/Edit.cshtml.cs
@@ -6,15 +6,72 @@ using RVfamcamp.Services;
 
 public class EditModel(DatabaseStatements db) : PageModel
 {
+    public required Reservation Reservation { get; set; }
+    public List<Lot>? Available_Lots { get; set; }
+    public required List<Lot>? ReservationLots { get; set; }
+    public bool datesAccepted { get; set; }
+
     [BindProperty]
-    public Reservation Reservation { get; set; }
+    public DateOnly StartDate { get; set; }
+
+    [BindProperty]
+    public DateOnly EndDate { get; set; }
+    [BindProperty]
+    public int reservationID { get; set; }
 
     public void OnGet(int id)
     {
         Reservation = db.GetReservationById(id);
+        reservationID = Reservation.reservationId;
+        StartDate = DateOnly.FromDateTime(Reservation.startDate);
+        EndDate = DateOnly.FromDateTime(Reservation.endDate);
     }
+    public IActionResult OnPostCheckAvailability()
+    {
+        var today = DateOnly.FromDateTime(DateTime.Today);
 
-    public IActionResult OnPost()
+        if (StartDate < today)
+        {
+            ModelState.AddModelError("", "Start date cannot be in the past.");
+        }
+
+        if (EndDate < today)
+        {
+            ModelState.AddModelError("", "End date cannot be in the past.");
+        }
+
+        if (EndDate < StartDate)
+        {
+            ModelState.AddModelError("", "End date cannot be before start date.");
+        }
+
+        if (!ModelState.IsValid)
+            return Page();
+
+        ReservationLots = (List<Lot>?)db.GetLotsByReservationId(reservationID);
+
+        // momentarily remove lots from reservation to help with availability check
+        db.ClearLotsFromReservation(reservationID);
+
+        // check for lot availability over new range
+        Available_Lots = (List<Lot>?)db.GetUnreservedLotsOverRange(StartDate, EndDate);
+        datesAccepted = true;
+        foreach (Lot lot in ReservationLots)
+        {
+            if (Available_Lots.Contains(lot))
+            {
+                datesAccepted = false;
+                ModelState.AddModelError("", "Lots is already reserved in that date range.");
+                break;
+
+            }
+            // Add lots back to reservation
+            db.AssignLotToReservation(lot.LotId, reservationID);
+        }
+
+        return Page();
+    }
+    public IActionResult OnPostSaveChanges()
     {
         if (!ModelState.IsValid)
         {
@@ -23,7 +80,7 @@ public class EditModel(DatabaseStatements db) : PageModel
         // TODO: Update database
         else
         {
-            
+            db.EditReservation(reservationID, StartDate, EndDate);
         }
 
         return RedirectToPage("Index");
